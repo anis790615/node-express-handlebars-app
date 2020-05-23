a5993fe7abec778c30e1b7fd893718ae738b9005
Anis Alkomem <anis.alkumaim@gmail.com>
2020-05-23 17:23:04 +0200

    Changed process.env.PORT line

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ app.use(express.static(path.join(__dirname, "public")));
 // Serving API
 app.use("/api/members", require("./routes/api/members"));
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
